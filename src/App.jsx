b69aba333d5c0902bf075a1ce82f04502416d3d4
Code-Chuks-droid <chuksgodgift262@gmail.com>
2026-01-28 12:36:01 +0100

    First_Update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="logo text-3xl font-bold text-red-500">
+        Welcome to Food Recipe
+      </h1>
+    </div>
+  );
+}
