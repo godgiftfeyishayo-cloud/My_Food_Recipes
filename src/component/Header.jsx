e41589e0b1eb23cc57c8f177a6484068edd8e777
Code-Chuks-droid <chuksgodgift262@gmail.com>
2026-02-03 14:18:07 +0100

    updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,17 @@
 import { TextAlignJustify } from "lucide-react";
 import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 export default function Header() {
+  const [openNav, setOpenNav] = useState(false);
   return (
     <header className="bg-yellow-400 p-4 shadow-md flex items-center gap-1.5 justify-between sticky top-0">
       <h1 className="text-3xl text-white text-center logo">Food Recipe Hub</h1>
-      <TextAlignJustify className="text-white" />
-      <Sidebar />
+      <TextAlignJustify
+        className="text-white"
+        onClick={() => setOpenNav(!openNav)}
+      />
+      <Sidebar isNavOpen={openNav} />
     </header>
   );
 }
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function 
-() {
+export default function Sidebar({ isNavOpen }) {
   return (
-    <aside className='fixed top-0 left-0 bg-blue-500 w-1/2 bottom-0 -translate-x-full'>
-        Sidebar
+    <aside
+      className={`fixed top-0 left-0 bg-amber-500 w-1/2 bottom-0 duration-700 ${isNavOpen ? "translate-x-0" : "-translate-x-full"}`}
+    >
+      Sidebar
     </aside>
-  )
+  );
 }
